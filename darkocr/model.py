d6dd0251182254afb665a2520368a4f2877cd697
Mateusz K <koldowskimateusz@gmail.com>
2018-11-11 19:08:49 +0100

    model improvement, hyperparameters, batch normalization

--- darkocr/model.py
+++ darkocr/model.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import time
 from tensorflow import keras
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense
+from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense, BatchNormalization, Activation
 
 
 class CNNModel:
@@ -11,25 +13,46 @@ class CNNModel:
 
         self.history = None
 
+        # hyper parameters
+        dropout = 0.2
+
         # build convolution network model
         self.model = keras.Sequential()
-        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu',
+
+        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False,
                               input_shape=(im_width, im_height, 1)))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation("relu"))
         self.model.add(MaxPool2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(Dropout(dropout))
+
+        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation("relu"))
         self.model.add(MaxPool2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(Dropout(dropout))
+
+        self.model.add(Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation("relu"))
         self.model.add(MaxPool2D(pool_size=(2, 2)))
+        self.model.add(Dropout(dropout))
+
         self.model.add(Flatten())
-        self.model.add(Dense(10, activation='relu'))
-        self.model.add(Dropout(0.5))
+        self.model.add(Dense(10, use_bias=False))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation("relu"))
+        self.model.add(Dropout(dropout))
         self.model.add(Dense(classes_count, activation='softmax'))
 
-        self.model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        self.model.compile(optimizer='nadam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     def fit(self, train_x, train_y, test_x, test_y):
+        start_time = time.time()
         self.history = self.model.fit(train_x, train_y, batch_size=512, validation_data=(test_x, test_y),
                                       epochs=30, verbose=1)
+        end_time = time.time()
+        print('Training time: {} s'.format(int(end_time - start_time)))
 
     def predict(self, data_x):
         return self.model.predict(data_x)
